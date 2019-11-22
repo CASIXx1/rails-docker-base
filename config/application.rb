af146f1f037e18d447c058aa4e19432f183bae8c
CASIXx1 <uplifting0760@gmail.com>
2019-11-23 03:10:48 +0900

    fixed rubocop errors

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,16 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.generators do |g|
-      g.test_framework :rspec,
-        # controller_specs: false,
-        # view_specs: false,
-        # helper_specs: false,
-        # routing_specs: false
-    end
+
+    config.generators.test_framework :rspec
+    # optional settings
+    #
+    # config.generators do |g|
+    #   g.test_framework :rspec
+    #   g.controller_specs: false,
+    #   g.view_specs: false,
+    #   g.helper_specs: false,
+    #   g.routing_specs: false
+    # end
   end
 end
